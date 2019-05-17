09cd76a5903a6a3255900f759d8f13bb5f740ddd
Wan <wnh3yang@gmail.com>
2019-05-17 10:57:32 +0800

    dns

--- socks.start.bat
+++ socks.start.bat
@@ -2,3 +2,4 @@
 ECHO Starting Socks Server...
 hide E:\workspace\networktunnel\venv\Scripts\twistd.exe -y E:\workspace\networktunnel\local.tac
 hide E:\workspace\networktunnel\ss_win_temp\ss_privoxy.exe -c E:\workspace\networktunnel\ss_win_temp\privoxy.conf
+hide E:\workspace\networktunnel\dns\dns2socks.exe 127.0.0.1:1080 8.8.8.8:53
